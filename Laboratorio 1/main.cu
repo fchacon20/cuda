87899695c909cf9297684de76ea07051c3c9adbc
Diego Wilhelm <diego.wilhelm.13@sansano.usm.cl>
2018-10-05 10:46:16 -0300

    main vacio

---
+++ Laboratorio 1/main.cu
@@ -0,0 +1,18 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <istream>
+#include <iterator>
+#include "functions.cuh"
+#include <stdio.h>
+#include <time.h>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
