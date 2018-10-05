07af2f656a25688f2b9d0821b2fa4829942bad65
Felipe <felipe.chacon.13@sansano.usm.cl>
2018-10-05 12:45:53 -0300

    Added function to plot

---
+++ Laboratorio 1/plotEuler.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+file = open("euler.txt", 'r')
+deltaT = 1
+
+for line in file:
+	line = line.split(" ")
+	for i in range(len(line)):
+		line[i] = float(line[i])
+	deltaT *= 0.1
+	y = np.linspace(0, 10, round(10 / deltaT,1))
+	plt.plot(line, y, 'ro')
+	plt.show()
