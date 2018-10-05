4440a0058454cf0f5996da81ad8a0ad274284ad8
Felipe <felipe.chacon.13@sansano.usm.cl>
2018-10-05 17:31:46 -0300

    Fixed 1a

--- Laboratorio 1/kernel.cu
+++ Laboratorio 1/kernel.cu
@@ -3,7 +3,8 @@
 #include "device_launch_parameters.h"
 #include <iostream>
 #include <math.h>
-#include <chrono>
+#include <time.h>
+#include <fstream>
 
 #define E 2.71828182845904523536
 
@@ -12,13 +13,11 @@ using namespace std;
 float* eulerMethodCPU(int t0, float y0, float deltaT) {
 	int n = int(10 / deltaT);
 	float* y = new float[n];
-	float sum;
+	float sum = 0.0;
 	
 	y[0] = y0;
 	for (int i = 1; i < n; i++)	{
-		sum = 0;
-		for (int j = 0; j < i; j++)
-			sum = sum + powf(E, -1 *j*deltaT);
+		sum = sum + powf(E, -1 *(i-1)*deltaT);
 		y[i] = y0 + deltaT * sum;
 	}
 	return y;
@@ -29,15 +28,26 @@ int main() {
 	
 	float* y;
 	float deltaT[6] = { 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001 };
-	chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
+	clock_t t1;
+	clock_t t2;
+	long duration;
 
-	for (int i = 0; i < 6; i++) 
+	ofstream outputFile;
+	outputFile.open("euler.txt");
+
+	for (int i = 0; i < 6; i++) {
+		t1 = clock();
 		y = eulerMethodCPU(0, -1, deltaT[i]);
-	
-	chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
-	long duration = chrono::duration_cast<chrono::microseconds>(t2 - t1).count();
-	cout << "Tiempo de ejecucion en CPU: " << duration << " [ms]" << endl;
+		t2 = clock();
+		duration = 1000*(double)(t2 - t1) / CLOCKS_PER_SEC;
+		cout << "[CPU] DeltaT = " << deltaT[i] << ": " << duration << " [ms]" << endl;
 
+		for (int j = 0; j < int(10 / deltaT[i])-1; j++)
+			outputFile << y[j] << " ";
+		outputFile << y[int(10 / deltaT[i]) - 1] << endl;
+	}	
+
+	outputFile.close();
 	delete[] y;
 	return 0;
 }
