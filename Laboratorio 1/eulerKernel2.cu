ce3ab6d14bcfb40b2a4868144c42e0f88da7ff9b
ivalenzu <menthos123@gmail.com>
2018-10-05 23:48:49 -0300

    Limpieza y diferenciacion de archivos por pregunta

---
+++ Laboratorio 1/eulerKernel2.cu
@@ -0,0 +1,14 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#include <fstream>
+
+#define E 2.71828182845904523536
+
+using namespace std;
+
+int main(){
+    return 0;
+}
