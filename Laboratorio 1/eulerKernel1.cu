c7644e50169b0e93d07cd0826207c9c320c37dc8
Felipe <felipe.chacon.13@sansano.usm.cl>
2018-10-07 19:06:58 -0300

    Delete[]

--- Laboratorio 1/eulerKernel1.cu
+++ Laboratorio 1/eulerKernel1.cu
@@ -5,67 +5,59 @@
 #include <math.h>
 #include <time.h>
 #include <fstream>
+#include <ctime>
+#include <iomanip>
 
 #define E 2.71828182845904523536
 
 using namespace std;
 
-void __global__ calculoGPU(float t0, float y0, float deltaT, int n, float * soluciones_GPU){
-    int tId = threadIdx.x + blockIdx.x * blockDim.x;
-    int i;
-    float sum = 0;
-    if(tId == 0)
-        soluciones_GPU[0] = y0;
-    else if(tId < n){
-        for(i = 1; i < tId; i++)
-            sum = sum + powf(E, -1 * (i - 1) * deltaT);
-        soluciones_GPU[tId] = (sum * deltaT) + y0;
-    }
+void __global__ calculoGPU(float t0, float y0, float deltaT, int n, float * soluciones_GPU) {
+	int tId = threadIdx.x + blockIdx.x * blockDim.x;
+	int i;
+	float sum = 0;
+	if (tId == 0)
+		soluciones_GPU[0] = y0;
+	else if (tId < n) {
+		for (i = 1; i < tId; i++)
+			sum = sum + powf(E, -1 * (i - 1) * deltaT);
+		soluciones_GPU[tId] = (sum * deltaT) + y0;
+	}
 }
 
-void eulerMethodGPU(float t0, float y0, float deltaT){
-    FILE * archivo;
-    float tN = 10;
-    int i;
-    cudaEvent_t ct1, ct2;
-    float dt;
-    int n = (tN - t0)/deltaT;
-    int block_size = 256;
-    int grid_size = (int)ceil((float)n / block_size);
-    float * soluciones = new float[n];
-    float * soluciones_GPU;
-    cudaMalloc(&soluciones_GPU, n * sizeof(float));
-    cudaEventCreate(&ct1);
-    cudaEventCreate(&ct2);
-    cudaEventRecord(ct1);
-    calculoGPU << <grid_size, block_size>> >(t0, y0, deltaT, n, soluciones_GPU);
-    cudaEventRecord(ct2);
-    cudaEventSynchronize(ct2);
-    cudaEventElapsedTime(&dt, ct1, ct2);
-    cudaMemcpy(soluciones, soluciones_GPU, n * sizeof(float), cudaMemcpyDeviceToHost);
+void eulerMethodGPU(float t0, float y0, float deltaT) {
+	float tN = 10;
+	cudaEvent_t ct1, ct2;
+	float dt;
+	int n = (int)((tN - t0) / deltaT);
+	int block_size = 256;
+	int grid_size = (int)ceil((float)n / block_size);
+	float * soluciones = new float[n];
+	float * soluciones_GPU;
+	cudaMalloc(&soluciones_GPU, n * sizeof(float));
+	cudaEventCreate(&ct1);
+	cudaEventCreate(&ct2);
+	cudaEventRecord(ct1);
+	calculoGPU << <grid_size, block_size >> > (t0, y0, deltaT, n, soluciones_GPU);
+	cudaEventRecord(ct2);
+	cudaEventSynchronize(ct2);
+	cudaEventElapsedTime(&dt, ct1, ct2);
+	cudaMemcpy(soluciones, soluciones_GPU, n * sizeof(float), cudaMemcpyDeviceToHost);
 
 	cout << "[GPU] DeltaT = " << deltaT << ": " << dt << " [ms]" << endl;
 
-    archivo = fopen("eulerGPU.txt", "a");
-    for(i = 0; i < n; i++){
-        if(i == n-1)
-            fprintf(archivo, "%f\n", soluciones[i]);
-        else
-            fprintf(archivo, "%f ", soluciones[i]);
-    }
-    fclose(archivo);
-    cudaFree(soluciones_GPU);
-    delete soluciones;
+	cudaFree(soluciones_GPU);
+	delete[] soluciones;
 }
 
 float* eulerMethodCPU(int t0, float y0, float deltaT) {
 	int n = int(10 / deltaT);
 	float* y = new float[n];
 	float sum = 0.0;
-	
+
 	y[0] = y0;
-	for (int i = 1; i < n; i++)	{
-		sum = sum + powf(E, -1 *(i-1)*deltaT);
+	for (int i = 1; i < n; i++) {
+		sum = sum + powf((float) E, -1 * (i - 1)*deltaT);
 		y[i] = y0 + deltaT * sum;
 	}
 	return y;
@@ -73,34 +65,28 @@ float* eulerMethodCPU(int t0, float y0, float deltaT) {
 
 
 int main() {
-	
+
 	float* y;
-	float deltaT[6] = { 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001 };
+	float deltaT[6] = { (float) 1e-1, (float) 1e-2, (float) 1e-3,
+						(float) 1e-4, (float) 1e-5, (float) 1e-6};
 	clock_t t1;
 	clock_t t2;
-	long duration;
-
-	ofstream outputFile;
-	outputFile.open("euler.txt");
+	double duration;
 
 	for (int i = 0; i < 6; i++) {
 		t1 = clock();
 		y = eulerMethodCPU(0, -1, deltaT[i]);
 		t2 = clock();
-		duration = 1000*(double)(t2 - t1) / CLOCKS_PER_SEC;
+		duration = 1000 * (double)(t2 - t1) / CLOCKS_PER_SEC;
 		cout << "[CPU] DeltaT = " << deltaT[i] << ": " << duration << " [ms]" << endl;
+	}
 
-		for (int j = 0; j < int(10 / deltaT[i])-1; j++)
-			outputFile << y[j] << " ";
-		outputFile << y[int(10 / deltaT[i]) - 1] << endl;
-	}	
 	cout << "---------------" << endl;
-	eulerMethodGPU(0, -1, 0.1);
-    eulerMethodGPU(0, -1, 0.01);
-    eulerMethodGPU(0, -1, 0.001);
-	eulerMethodGPU(0, -1, 0.0001);
-	
-	outputFile.close();
+	eulerMethodGPU(0, -1, deltaT[0]);
+	eulerMethodGPU(0, -1, deltaT[1]);
+	eulerMethodGPU(0, -1, deltaT[2]);
+	eulerMethodGPU(0, -1, deltaT[3]);
+
 	delete[] y;
 	return 0;
 }
