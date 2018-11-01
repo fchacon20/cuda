6ff2eb0ecaaf01c00bd3159b4c93f6d1c85dff1a
ivalenzu <menthos123@gmail.com>
2018-11-01 03:10:57 -0300

    Implementacion letra a, aun no se rutea si hace bien cada step

---
+++ Laboratorio 2/kernela.cu
@@ -0,0 +1,236 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <istream>
+#include <iterator>
+
+using namespace std;
+
+__global__ void timeStepSoA(int *devF, int *devF1, int N, int M){
+    int tId = threadIdx.x + blockIdx.x * blockDim.x;
+
+	if (tId < N*M) {
+		int f0, f1, f2, f3, x, y;
+		f0 = devF[tId];
+		f1 = devF[tId + N*M];
+		f2 = devF[tId + N*M*2];  
+		f3 = devF[tId + N*M*3];
+		x = tId / M;
+		y = tId % M;
+		// Collisions
+		if (f0 == 1 && f1 == 0 && f2 == 1 && f3 == 0) {
+			f0 = 0;
+			f2 = 0;
+			f1 = 1;
+			f3 = 1;
+		} else if (f0 == 0 && f1 == 1 && f2 == 0 && f3 == 1) {
+			f0 = 1;
+			f2 = 1;
+			f1 = 0;
+			f3 = 0;
+		}
+
+		//Streaming
+		if(f0 == 1){
+			devF[tId] = 0;
+			if(y == M-1)
+				devF1[x*M] = 1;
+			else
+				devF1[x*M + y + 1] = 1;
+		}
+		if(f1 == 1){  
+			devF[tId + N*M] = 0;
+			if(x == 0)
+				devF1[(N-1)*M + y + N*M] = 1;
+			else
+				devF1[(x-1)*M + y + N*M] = 1;
+		}
+		if(f2 == 1){
+			devF[tId + N*M*2] = 0;
+			if(y == 0)
+				devF1[x*M + M-1 + N*M*2] = 1;
+			else
+				devF1[x*M + y - 1 + N*M*2] = 1;
+		}
+		if(f3 == 1){
+			devF[tId + N*M*3] = 0;
+			if(x == N-1)
+				devF1[y + N*M*3] = 1;
+			else
+				devF1[(x+1)*M + y + N*M*3] = 1;
+		} 
+	}
+}
+
+__global__ void timeStepAoS(int *devF, int *devF1, int N, int M){
+	int tId = threadIdx.x + blockIdx.x * blockDim.x;
+	
+	if(tId < N*M*4 && tId % 4 == 0){
+		int f0, f1, f2, f3, x, y;
+		f0 = tId;
+		f1 = tId + 1;
+		f2 = tId + 2;
+		f3 = tId + 3;
+		x = (tId / 4) / M;
+		y = (tId / 4) % M;
+		// Collisions
+		if (f0 == 1 && f1 == 0 && f2 == 1 && f3 == 0) {
+			f0 = 0;
+			f2 = 0;
+			f1 = 1;
+			f3 = 1;
+		} else if (f0 == 0 && f1 == 1 && f2 == 0 && f3 == 1) {
+			f0 = 1;
+			f2 = 1;
+			f1 = 0;
+			f3 = 0;
+		}
+		// Streaming
+		if(f0 == 1){
+			devF[tId] = 0;
+			if(y == M-1)
+				devF1[x*M*4] = 1;
+			else
+				devF1[(x*M + y + 1)*4] = 1;
+		}
+		if(f1 == 1){  
+			devF[tId + 1] = 0;
+			if(x == 0)
+				devF1[((N-1)*M + y)*4 + 1] = 1;
+			else
+				devF1[((x-1)*N + y)*4 + 1] = 1;
+		}
+		if(f2 == 1){
+			devF[tId + 2] = 0;
+			if(y == 0)
+				devF1[(x*M + M-1)*4 + 2] = 1;
+			else
+				devF1[(x*M + y - 1)*4 + 2] = 1;
+		}
+		if(f3 == 1){
+			devF[tId + 3] = 0;
+			if(x == N-1)
+				devF1[y*4 + 3] = 1;
+			else
+				devF1[((x+1)*M + y)*4 + 3] = 1;
+		}
+	}
+}
+
+__global__ void FinalStep (int* devF, int* devF1, int N, int M) {
+	int tId = threadIdx.x + blockIdx.x * blockDim.x;
+
+	if (tId < N*M*4 && devF1[tId] == 1)
+		devF[tId] = 1;
+}
+
+int * SoAGen(vector<vector<string> > lines, int N, int M){
+	int * SoA_array = new int [N*M*4];
+	int contador = 0;
+	for (int i = 1; i <= 4; i++){
+		for (int j = 0; j < N*M; j++){
+			SoA_array[contador] = stoi(lines[i][j]);
+			contador++;
+		}
+	}
+	return SoA_array;
+}
+
+int * AoSGen(vector<vector<string> > lines, int N, int M){
+	int * AoS_array = new int [N*M*4];
+	int contador = 0;
+	for (int i = 0; i < N*M; i++){
+		for (int j = 1; j <= 4; j++){
+			AoS_array[contador] = stoi(lines[j][i]);
+			contador++;
+		}
+	}
+	return AoS_array;
+}
+
+int main(){
+
+	ifstream file("initial.txt");
+	int N, M, count = 1;
+	string line;
+	vector<vector<string> > lines;
+	cudaEvent_t ct1, ct2;
+	float durationGPU;
+	int blockSize = 256;
+	int gridSize;
+	int * devSoa;
+	int * devAos;
+	int * devF1;
+	int * SoA;
+	int * AoS;
+
+	while (getline(file, line)) {
+		cout << "Cargando linea " << count << " de 5." << endl;
+		istringstream iss(line);
+		lines.push_back(vector<string>(istream_iterator<string> { iss }, istream_iterator<string>()));
+		count++;
+	}
+
+	file.close();
+
+	N = stoi(lines[0][0]);
+	M = stoi(lines[0][1]);
+	gridSize = (int)ceil((float)N*M / blockSize);
+
+	SoA = SoAGen(lines, N, M);
+	AoS = AoSGen(lines, N, M);
+	cudaMalloc(&devSoa, N*M*4 * sizeof(int));
+	cudaMalloc(&devF1, N*M*4 * sizeof(int));
+	cudaMalloc(&devAos, N*M*4 * sizeof(int));
+	cudaMemcpy(devSoa, SoA, N*M*4 * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(devAos, AoS, N*M*4 * sizeof(int), cudaMemcpyHostToDevice);
+	cudaEventCreate(&ct1);
+	cudaEventCreate(&ct2);
+	cudaEventRecord(ct1);
+
+	for(int i = 0; i < 2; i++){
+		cudaMemset(&devF1, 0, N*M*4 * sizeof(int));
+		timeStepSoA << <gridSize, blockSize >> > (devSoa, devF1, N, M);
+		cudaDeviceSynchronize();
+		FinalStep << <gridSize, blockSize >> > (devSoa, devF1, N, M);
+	}
+
+	cudaEventRecord(ct2);
+	cudaEventSynchronize(ct2);
+	cudaEventElapsedTime(&durationGPU, ct1, ct2);
+
+	cudaMemcpy(SoA, devSoa, N*M*4 * sizeof(int), cudaMemcpyDeviceToHost);
+
+	cout << "Tiempo de ejecucion SoA: " << durationGPU << "[ms]" << endl;
+
+	cudaEventRecord(ct1);
+
+	for(int i = 0; i < 2; i++){
+		cudaMemset(&devF1, 0, N*M*4 * sizeof(int));
+		timeStepAoS << <gridSize, blockSize >> > (devAos, devF1, N, M);
+		cudaDeviceSynchronize();
+		FinalStep << <gridSize, blockSize >> > (devAos, devF1, N, M);
+	}
+	cudaEventRecord(ct2);
+	cudaEventSynchronize(ct2);
+	cudaEventElapsedTime(&durationGPU, ct1, ct2);
+
+	cudaMemcpy(AoS, devAos, N*M*4 * sizeof(int), cudaMemcpyDeviceToHost);
+
+	cout << "Tiempo de ejecucion AoS: " << durationGPU << "[ms]" << endl;
+
+	cout << "Programa terminado" << endl;
+
+	delete[] AoS;
+	delete[] SoA;
+	cudaFree(devF1);
+	cudaFree(devSoa);
+	cudaFree(devAos);
+
+    return 0;
+}
