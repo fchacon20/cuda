3d8f271c1372666a7fb2b7bfe7b48de31617a137
Felipe Chacón <felipe.chacon.ossa@gmail.com>
2018-12-01 16:49:36 -0300

    Agregado el kernelB y una forma de comprobar que el kernel funciona

--- Laboratorio 3/kernel.cu
+++ Laboratorio 3/kernel.cu
@@ -22,40 +22,44 @@ comprobar si el resultado es correcto cuando todos los valores en b sean 10^4.
 */
 
 __global__ void kernelA(int *A, int*x, int*b, int N) {
+	// Este kernel utiliza N x N = 10^8 hebras. Cada hebra esta asociada a un elemento a_i,j de la matriz A
+	// multiplicándolo por el valor x_j correspondiente y sumando este resultado al elemento en la i-ésima
+	// posición del vector b.
+	
 	int tId = threadIdx.x + blockIdx.x * blockDim.x;
 
-	int i = tId % N;
-	int j = tId / N;
+	int i = tId / N;
+	int j = tId % N;
 	if (i < N && j < N) {
-		//atomicAdd(A[i][j] * x[j], b[i]);
+		int mult = A[j+i*N] * x[j];
+		atomicAdd(&b[i], mult);
 	}
-	/*
-	Este kernel utiliza N x N = 10^8 hebras. Cada hebra esta asociada a un elemento a_i,j de la matriz A
-	multiplicándolo por el valor x_j correspondiente y sumando este resultado al elemento en la i-ésima
-	posición del vector b.
-	*/
 }
 
 __global__ void kernelx(int *A, int*x, int*b, int N) {
-	/*
-	Este kernel utiliza N = 10^4 hebras. Cada hebra esta asociada a un elemento x_j del vector x,
-	sumando a cada uno de los N valores b_i	, la multiplicacion de dicho x_j
-	por el correspondiente elemento a_i,j
-	*/
+	// Este kernel utiliza N = 10^4 hebras. Cada hebra esta asociada a un elemento x_j del vector x,
+	// sumando a cada uno de los N valores b_i	, la multiplicacion de dicho x_j
+	// por el correspondiente elemento a_i,j
 
 	int tId = threadIdx.x + blockIdx.x * blockDim.x;
 	if (tId < N) {
-		for (int j = 0; j < N; j++) {
-			int mult = x[tId] * A[j];
-			atomicAdd(&b[tId], mult);
+		int j = tId;
+		for (int i = 0; i < N; i++) {
+			int mult = x[j] * A[i*N + j];
+			atomicAdd(&b[i], mult);
 		}
 	}
 }
 
-
 __global__ void kernelb(int *A, int*x, int*b, int N) {
-
-
+	int tId = threadIdx.x + blockIdx.x * blockDim.x;
+	if (tId < N) {
+		int i = tId;
+		for (int j = 0; j < N; j++) {
+			int mult = x[j] * A[j+i*N];
+			b[i] += mult;
+		}
+	}
 }
 
 __global__ void kernelRed(int *A, int*x, int*b, int N) {
@@ -74,38 +78,64 @@ void fillArray(int *a, int n) {
 
 int main() {
 
-	int N = 1e1;
-	int M = 1e1;
+	int N = 2;
+	int M = 2;
 	int* A = new int[N*M];
-	int* X = new int[N];
+	int* X = new int[M];
 	int* B = new int[N];
-	
+
 	int *devA, *devX, *devB;
 	cudaMalloc(&devA, N*M * sizeof(int));
-	cudaMalloc(&devX, N * sizeof(int));
+	cudaMalloc(&devX, M * sizeof(int));
 	cudaMalloc(&devB, N * sizeof(int));
 
-	fillArray(A, N*M);
-	fillArray(X, N);
-	
+	//fillArray(A, N*M);
+	//fillArray(X, N);
+
+	// Test 2x2
+	A[0] = 2; A[1] = -1;
+	A[2] = 3; A[3] = 5;
+	X[0] = 3; X[1] = -4;
+	// Resultado es (10, -11)^T
+
 	cudaMemcpy(devA, A, N*M * sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(devX, X, N * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(devX, X, M * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemset(devB, 0, N * sizeof(int));
 
 	int block_size = 256;
 
-	// KernelX
-	int grid_size = (int)ceil((float)N / block_size);
-
-	kernelx << <grid_size, block_size >> > (devA, devX, devB, N);
-
+	// KernelA
+	int grid_sizeA = (int)ceil((float)N*M / block_size);
+	kernelA << <grid_sizeA, block_size >> > (devA, devX, devB, N);
 	cudaMemcpy(B, devB, N * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemset(devB, 0, N * sizeof(int));
 
+	cout << "KernelA: " << endl;;
 	for (int i = 0; i < N; i++)
-	{
 		cout << B[i] << endl;
-	}
-	
+	cout << endl;
 
+	// KernelX
+	int grid_sizeX = (int)ceil((float)M / block_size);
+	kernelx << <grid_sizeX, block_size >> > (devA, devX, devB, M);
+	cudaMemcpy(B, devB, M * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemset(devB, 0, N * sizeof(int));
+
+	cout << "KernelX: " << endl;;
+	for (int i = 0; i < N; i++)
+		cout << B[i] << endl;
+	cout << endl;
+
+	// KernelB
+	int grid_sizeB = (int)ceil((float)N / block_size);
+	kernelb << <grid_sizeB, block_size >> > (devA, devX, devB, N);
+	cudaMemcpy(B, devB, N * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemset(devB, 0, N * sizeof(int));
+
+	cout << "KernelB: " << endl;;
+	for (int i = 0; i < N; i++)
+		cout << B[i] << endl;
+	cout << endl;
+	
 	return 0;
 }
