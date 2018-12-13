3dce0dd8c58648fab80483b5fbe42023a4e98795
fchacon20 <felipe.chacon.13@sansano.usm.cl>
2018-12-13 17:32:50 -0300

    Puntos de chevyshev agregados

--- Proyecto/main.cpp
+++ Proyecto/main.cpp
@@ -4,10 +4,11 @@
 
 using namespace std;
 
-void initialPoints(float *x, float *y, int n){
-    for (int i = 0; i < n; ++i) {
-        x[i] = i;
-        y[i] = 3*x[i]+(float)pow(x[i],2)+1;
+void initialPoints(double *x, double *y, int n, int a, int b){
+    for (int i = 1; i <= n; ++i) {
+        x[i-1] = (double)(a+b)/2 + (double)(((b-a)/2.0)*cos((2.0*i-1.0)*M_PI/((double)2.0*n)));
+        //y[i-1] = 3*x[i-1]+(double)pow(x[i-1],2)+1;
+        y[i-1] = cos(x[i-1]);
     }
 }
 
@@ -15,12 +16,14 @@ int main() {
 
     int n = 30;
     int m = 1000000;
-    float* x = new float[n];
-    float* y = new float[n];
-    float* xn = new float[m];
-    float* yn = new float[m];
+    int a = 0;
+    int b = 100;
+    double* x = new double[n];
+    double* y = new double[n];
+    double* xn = new double[m];
+    double* yn = new double[m];
 
-    initialPoints(x, y, n);
+    initialPoints(x, y, n, a, b);
 
     // Saving input
     ofstream outfile("./initialPoints.txt");
@@ -34,11 +37,11 @@ int main() {
 
     clock_t begin = clock();
 
-    float num = 1, den = 1;
-    float frac, result = 0;
+    double num = 1, den = 1;
+    double frac, result = 0;
 
     for (int k = 1; k <= m; ++k) {
-        xn[k-1] = (float) 0.00003*k;
+        xn[k-1] = ((double)(b-a)/(m))*k;
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (i != j) {
@@ -67,6 +70,11 @@ int main() {
         outfile2 << yn[i] << ",";
     outfile2 << yn[m-1] << "\n";
     outfile2.close();
+
+    delete x;
+    delete y;
+    delete xn;
+    delete yn;
 
     return 0;
 }
